1c8434a0701ee9ea001c0725967f52ee170d966d
Vicko Lukšić <vickoluksic@atron.de>
2020-12-01 21:39:43 +0100

    Test of new ssh key commit

---
+++ generateKeys.js
@@ -0,0 +1,29 @@
+const crypto = require('crypto')
+const path = require('path')
+const fs = require('fs')
+
+function encrypt(toEncrypt, relativeOrAbsolutePathToPublicKey) {
+const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+const publicKey = fs.readFileSync(absolutePath, 'utf8')
+const buffer = Buffer.from(toEncrypt, 'utf8')
+const encrypted = crypto.publicEncrypt(publicKey, buffer)
+return encrypted.toString('base64')}
+
+function decrypt(toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
+const absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey)
+const privateKey = fs.readFileSync(absolutePath, 'utf8')
+const buffer = Buffer.from(toDecrypt, 'base64')
+const decrypted = crypto.privateDecrypt(
+  {
+    key: privateKey.toString(),
+    passphrase: '',
+  },
+  buffer,
+)
+return decrypted.toString('utf8')}
+
+const enc = encrypt('hello', `<public.pem>`)
+console.log('enc', enc)
+
+const dec = decrypt(enc, `<private.pem>`)
+console.log('dec', dec)
